8edc4ef946c13f94a8cec15eb2bd6ff17643248d
hschuler13 <hschul04@gmail.com>
2025-03-18 09:24:33 -0500

    minor adjustments for sidebar visuals (fixed hover color change)

--- with-mongodb-app/app/dashboard/page.tsx
+++ with-mongodb-app/app/dashboard/page.tsx
@@ -16,7 +16,7 @@ import {
 
 export default function Page() {
   return (
-    <>{/*<header className="flex h-full shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <><header className="flex h-full shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 ">
         <Separator orientation="vertical" className="mr-2 h-4" />
         <Breadcrumb>
@@ -40,6 +40,6 @@ export default function Page() {
           <div className="aspect-video rounded-xl bg-muted/50" />
         </div>
         <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      </div>*/}</>
+      </div></>
   )
 }
