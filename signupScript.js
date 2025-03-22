315b8f07d37644800f2bc5835fe981def50a6e74
ny0o0om <nora.pray8@gmail.com>
2025-03-22 00:56:48 -0500

    Working on Password Confirmation

--- signupScript.js
+++ signupScript.js
@@ -1,7 +1,9 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
 var express = require("express");
 var bodyParser = require("body-parser");
 
-const mongoose = require("mongoose");
 const { match } = require("assert");
 mongoose.connect(
   "mongodb+srv://userLogin:y7NaEfDttsDu6hgJ@cluster0.w7pqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
@@ -10,6 +12,19 @@ mongoose.connect(
 var db = mongoose.connection;
 
 var app = express();
+
+var userSchema = new Schema({
+  userName: { type: String, default: "NULL" },
+  userEmail: { type: String, default: "NULL" },
+  userAge: { type: Number, min: 18, index: true },
+  userBio: { type: String, match: /[a-zA-Z ]/ },
+  userDate: { type: Date, default: Date.now },
+  userPass: { type: String, default: "NULL" },
+});
+
+var userModel = mongoose.model("User", userSchema);
+
+
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -20,15 +35,15 @@ app.use(
 );
 
 app.post("/sign_up", function (req, res) {
-  var name = req.body.name;
-  var email = req.body.email;
-  var pass = req.body.password;
+  var userName = req.body.userName;
+  var userEmail = req.body.userEmail;
+  var userPass = req.body.userPass;
 
-  var data = {
-    name: name,
-    email: email,
-    password: pass,
-  };
+  var data = new userModel({
+    userName: userName,
+    userEmail: userEmail,
+    userPass: userPass,
+  });
 
   db.collection("userLogin").insertOne(data, function (err, collection) {
     if (err) throw err;
