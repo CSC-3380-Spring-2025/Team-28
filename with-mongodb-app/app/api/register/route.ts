74060c6def0b4eebd3ef1fa8d2edbbcdf48246f2
hschuler13 <hschul04@gmail.com>
2025-04-11 08:40:38 -0500

    added simple login system

---
+++ with-mongodb-app/app/api/register/route.ts
@@ -0,0 +1,30 @@
+import connectionToDatabase from "@/lib/mongoose"
+import User from "@/models/user"
+import bcrypt from "bcrypt"
+import { NextResponse } from "next/server"
+
+export async function POST(request: { json: () => PromiseLike<{ name: any; email: any; password: any }> | { name: any; email: any; password: any } }) {
+    try{
+        connectionToDatabase()
+        const {name, email, password} = await request.json()
+        const hobbies = [""]
+        const schedule = ""
+        const userExist = await User.findOne({email})
+        if(userExist){
+            return NextResponse.json({error: "user already exists"})
+        }
+        const hashPass = await bcrypt.hash(password, 10)
+        const newUser = new User({
+            name, 
+            email,
+            password: hashPass,
+            hobbies,
+            schedule
+        })
+        await newUser.save()
+        return NextResponse.json({message: "user registered", status: 201})
+    } catch(err){
+        return NextResponse.json({error: "error in server", status: 500})
+    }
+}
+
