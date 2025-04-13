3ec41bcb8135056eb14acd8c8fd6f2f52ad3a48b
ny0o0om <nora.pray8@gmail.com>
2025-04-12 22:32:26 -0500

    setup basic frontend, front and signup needs to be tweeked a bit

--- with-mongodb-app/app/register/page.tsx
+++ with-mongodb-app/app/register/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -7,30 +7,63 @@ export default function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const router = useRouter()
+  const router = useRouter();
 
-    const handleSubmit = async (e: { preventDefault: () => void; }) => {
-        e.preventDefault()
-        const response = await axios.post('/api/register', {name, email, password})
-        console.log(response)
-        router.push('/login')
-    }
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const response = await axios.post("/api/register", {
+      name,
+      email,
+      password,
+    });
+    console.log(response);
+    router.push("/login");
+  };
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <h1 className="font-bold text-6xl p-10">Create Account</h1>
       <form onSubmit={handleSubmit}>
+        <label className="h-10 w-15 p-2">Name</label>
         <div>
-          <label>Name</label>
-          <input type="text" placeholder="enter name" onChange={(e) => setName(e.target.value)}></input>
+          <input
+            className="bg-[#D9D9D9] rounded-md text-black h-10 p-2"
+            type="text"
+            placeholder="enter name"
+            onChange={(e) => setName(e.target.value)}
+          ></input>
         </div>
+        <label className="h-10 w-15 p-2">Email</label>
         <div>
-          <label>Email</label>
-          <input type="email" placeholder="enter email" onChange={(e) => setEmail(e.target.value)}></input>
+          <input
+            className="bg-[#D9D9D9] rounded-md text-black h-10 w-15 p-2"
+            type="email"
+            placeholder="enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
         </div>
+        <label className="h-10 w-15 p-2">Password</label>
         <div>
-          <label>Password</label>
-          <input type="password" placeholder="enter password" onChange={(e) => setPassword(e.target.value)}></input>
+          <input
+            className="bg-[#D9D9D9] rounded-md text-black h-10 w-15 p-2"
+            type="password"
+            placeholder="enter password"
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
         </div>
-        <button type="submit">Register</button>
+        <button
+          type="submit"
+          className="bg-black rounded-md text-white text-xl h-10 w-20 justify-center"
+        >
+          Sign Up
+        </button>
       </form>
     </div>
   );
