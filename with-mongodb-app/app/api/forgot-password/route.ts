7f377557e6b581eb1c5d8653ddbc18a27500db10
ny0o0om <nora.pray8@gmail.com>
2025-04-19 21:14:38 -0500

    Update password is working but it needs to be bug tested first and I will still need to work on figuring out password confirmation

--- with-mongodb-app/app/api/forgot-password/route.ts
+++ with-mongodb-app/app/api/forgot-password/route.ts
@@ -1,1 +1,27 @@
-// This needs to be worked on
+import connectionToDatabase from "@/lib/mongoose";
+import User from "@/models/user";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function POST(request: {
+  json: () =>
+    | PromiseLike<{ name: any; email: any; password: any }>
+    | { name: any; email: any; password: any };
+}) {
+  try {
+    connectionToDatabase();
+    const { email, password } = await request.json();
+    const hashPass = await bcrypt.hash(password, 10);
+    const filter = { email: email };
+    const update = { password: hashPass };
+
+    const userExist = await User.updateOne(filter, update);
+    if (userExist) {
+      return NextResponse.json({ error: "user already exists" });
+    }
+
+    return NextResponse.json({ message: "user registered", status: 201 });
+  } catch (err) {
+    return NextResponse.json({ error: "error in server", status: 500 });
+  }
+}
