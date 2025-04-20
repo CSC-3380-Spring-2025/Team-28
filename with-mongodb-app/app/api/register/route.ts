6fb981e6b13b9b00d0a06e6a3e590ee713a57979
ny0o0om <nora.pray8@gmail.com>
2025-04-19 21:01:12 -0500

    able to connect to server and find user, but still need to fix changing password

--- with-mongodb-app/app/api/register/route.ts
+++ with-mongodb-app/app/api/register/route.ts
@@ -1,32 +1,35 @@
-import connectionToDatabase from "@/lib/mongoose"
-import User from "@/models/user"
-import bcrypt from "bcrypt"
-import { NextResponse } from "next/server"
+import connectionToDatabase from "@/lib/mongoose";
+import User from "@/models/user";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
 
-export async function POST(request: { json: () => PromiseLike<{ name: any; email: any; password: any }> | { name: any; email: any; password: any } }) {
-    try{
-        connectionToDatabase()
-        const {name, email, password} = await request.json()
-        const hobbies = [""]
-        const schedule = ""
-        const reminder = ["", "", ""]
-        const userExist = await User.findOne({email})
-        if(userExist){
-            return NextResponse.json({error: "user already exists"})
-        }
-        const hashPass = await bcrypt.hash(password, 10)
-        const newUser = new User({
-            name, 
-            email,
-            password: hashPass,
-            hobbies,
-            schedule,
-            reminder
-        })
-        await newUser.save()
-        return NextResponse.json({message: "user registered", status: 201})
-    } catch(err){
-        return NextResponse.json({error: "error in server", status: 500})
+export async function POST(request: {
+  json: () =>
+    | PromiseLike<{ name: any; email: any; password: any }>
+    | { name: any; email: any; password: any };
+}) {
+  try {
+    connectionToDatabase();
+    const { name, email, password } = await request.json();
+    const hobbies = [""];
+    const schedule = "";
+    const reminder = ["", "", ""];
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return NextResponse.json({ error: "user already exists" });
     }
+    const hashPass = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      email,
+      password: hashPass,
+      hobbies,
+      schedule,
+      reminder,
+    });
+    await newUser.save();
+    return NextResponse.json({ message: "user registered", status: 201 });
+  } catch (err) {
+    return NextResponse.json({ error: "error in server", status: 500 });
+  }
 }
-
