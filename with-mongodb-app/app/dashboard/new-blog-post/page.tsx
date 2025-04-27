7eedbc4459cd1376426b357bd1402cc6cb53001f
KhalilE792 <khalilecollege@gmail.com>
2025-04-26 21:45:34 -0500

    starting adding backend for submitting new blog post

--- with-mongodb-app/app/dashboard/new-blog-post/page.tsx
+++ with-mongodb-app/app/dashboard/new-blog-post/page.tsx
@@ -21,7 +21,22 @@ export default function newBlogPost() {
     }
 
     return (<>
-
+      <div className="px-10">
+        <div className="pt-[1.5vh] pb-[1.5vh]">
+          <h1 className="font-bold text-2xl">Blog Post</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-4 relative w-full max-w-full items-start">
+          <h3 className="font-bold text-md pb-[1vh] col-span-3">Hobbies</h3>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter title" onChange={(e) => setTitle(e.target.value)}></input>
+            <input type="text" placeholder="Enter date" onChange={(e) => setDate(e.target.value)}></input>
+            <input type="text" placeholder="Enter post text" onChange={(e) => setContent(e.target.value)}></input>
+            <input type="text" placeholder="Enter image link" onChange={(e) => setImage(e.target.value)}></input>
+            /**handle blog ID */
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
     </>)
 
 }
