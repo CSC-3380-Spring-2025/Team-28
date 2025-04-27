1bfda1ce7b5d6d24095e993fd6a37cee4ddf76e1
ny0o0om <nora.pray8@gmail.com>
2025-04-26 22:16:17 -0500

    setup very basic frontend for the supplies page, but the backend needs to be setup so that the page can be filled out

--- with-mongodb-app/app/supplies/page.tsx
+++ with-mongodb-app/app/supplies/page.tsx
@@ -7,6 +7,75 @@ import Link from "next/link";
 export default function Supplies() {
   return (
     <div>
+      <h1 className="p-8 font-bold text-7xl mb-9">Supplies</h1>
+      <div className="grid-flow-col grid-rows-3 gap-4 flex">
+        <div className="border-4 border-black rounded-md row-span-3 ml-5">
+          <table className="border-collapse border border-gray-400">
+            <thead>
+              <tr className="border-black border-4 bg-black text-white">
+                <th>Supply Name</th>
+                <th>Amount</th>
+                <th>Cost Per Unit</th>
+                <th>Total Cost</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+              </tr>
+              <tr>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+              </tr>
+              <tr>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+              </tr>
+              <tr>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+              </tr>
+              <tr>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+              </tr>
+              <tr>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+              </tr>
+              <tr>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+                <td className="border-black border-4">Temp</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <div className="border-4 border-black rounded-md text-black col-span-2 mb-1">
+            <h2 className="bg-black text-white">Supply Alert</h2>
+            <div>Temp</div>
+          </div>
+          <div className="border-4 border-black rounded-md text-black col-span-2 row-span-2 mt-2 mb-1">
+            <h2 className="bg-black text-white">Financial Chart</h2>
+            <div>Temp</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
