497f30d1622a1baa70ba5a2791a23a7d22e139cf
ny0o0om <nora.pray8@gmail.com>
2025-05-01 19:31:54 -0500

    worked on frontend some to integrate with back end better

--- with-mongodb-app/app/supplies/page.tsx
+++ with-mongodb-app/app/supplies/page.tsx
@@ -3,68 +3,55 @@
 import { Separator } from "@/components/ui/separator";
 import React from "react";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import dynamic from "next/dynamic";
+import { teardownTraceSubscriber } from "next/dist/build/swc/generated-native";
+import { table } from "console";
+
 
 export default function Supplies() {
+  // need to import the component dynamically as it uses the 'window' property
+  // to calculate its dimensions and listen to user input events - which are only
+  // available in the browser
+  // if you have found a better way of adding the component in next, please create a new
+  // issue ticket so we can update the example!
+  const ActiveTable = dynamic(
+    () => import("active-table-react").then((mod) => mod.ActiveTable),
+    {
+      ssr: false,
+    }
+  );
+
+  // content is an example of passing a state object into the property
+  // headerStyles is an example of passing an object directly into the property
   return (
-    <div>
+    <div className="p-8">
       <h1 className="p-8 font-bold text-7xl mb-9">Supplies</h1>
       <div className="grid-flow-col grid-rows-3 gap-4 flex">
-        <div className="border-4 border-black rounded-md row-span-3 ml-5">
-          <table className="border-collapse border border-gray-400">
-            <thead>
-              <tr className="border-black border-4 bg-black text-white">
-                <th>Supply Name</th>
-                <th>Amount</th>
-                <th>Cost Per Unit</th>
-                <th>Total Cost</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-              </tr>
-              <tr>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-              </tr>
-              <tr>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-              </tr>
-              <tr>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-              </tr>
-              <tr>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-              </tr>
-              <tr>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-              </tr>
-              <tr>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-                <td className="border-black border-4">Temp</td>
-              </tr>
-            </tbody>
-          </table>
+        <div>
+          <ActiveTable
+            id="active-table"
+            isHeaderTextEditable={false}
+            displayAddNewColumn={false}
+            columnDropdown={{ displaySettings: { isAvailable: false } }}
+            tableStyle={{ borderRadius: "5px", border: "unset" }}
+            stripedRows={{odd: {"backgroundColor": ""}, even: {backgroundColor: "#ebebeb7a"}}}
+            headerStyles={{
+              default: {
+                backgroundColor: "#000000",
+                color: "white",
+                borderRight: "1px solid #00000029",
+              },
+            }}
+            data={[
+              ["Supply Name", "Cost per Unit", "Amount", "Total"],
+              ["Glue", "3", "4"],
+            ]}
+          />
         </div>
+
         <div>
           <div className="border-4 border-black rounded-md text-black col-span-2 mb-1">
             <h2 className="bg-black text-white">Supply Alert</h2>
