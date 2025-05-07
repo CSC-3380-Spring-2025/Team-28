2532f512196727dc673d8a5959e8a2d47065f625
hschuler13 <hschul04@gmail.com>
2025-05-07 15:21:25 -0500

    calendar page - fixed hobby fetching, added comments

---
+++ with-mongodb-app/app/api/hobbyFetch/route.ts
@@ -0,0 +1,18 @@
+import connectionToDatabase from "@/lib/mongoose"
+import User from "@/models/user"
+import { NextResponse } from "next/server"
+
+//This will fetch the hobbies associated with the user and return them
+export async function GET(){
+    try{
+        //Connect to MongoDB
+        await connectionToDatabase()
+        //Find the correct user document and get the corresponding hobbies array
+        const hobbies = await User.findOne({email: process.env.NEXT_PUBLIC_LOGGED_IN_USER}, {hobbies: 1})
+        //Return the hobbies associated with the user
+        return NextResponse.json({message: "hobby added", status: 201, hobbies: hobbies})
+    } catch(err) {
+        //Log error if something goes wrong in this process 
+        return NextResponse.json({message: "uh oh errorrr", status: 500})
+    }
+}
